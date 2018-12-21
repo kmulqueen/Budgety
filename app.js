e83ab62254aee35deee7dd75542c50ab752c38c2
Kyle Mulqueen <kmulqdrums710@gmail.com>
2018-12-20 19:59:16 -0700

    income and expenses now showing on ui.

--- app.js
+++ app.js
@@ -70,7 +70,9 @@ var uiController = (function() {
     inputType: ".add__type",
     inputDesc: ".add__description",
     inputValue: ".add__value",
-    inputBtn: ".add__btn"
+    inputBtn: ".add__btn",
+    incomeContainer: ".income__list",
+    expensesContainer: ".expenses__list"
   };
 
   return {
@@ -81,6 +83,30 @@ var uiController = (function() {
         description: document.querySelector(DOMStrings.inputDesc).value,
         value: document.querySelector(DOMStrings.inputValue).value
       };
+    },
+    addListItem: (obj, type) => {
+      var html, newHtml, element;
+      // Create HTML string w/ placeholder text
+      switch (type) {
+        case "inc":
+          element = DOMStrings.incomeContainer;
+          html = `<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
+          break;
+        case "exp":
+          element = DOMStrings.expensesContainer;
+          html = `<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
+          break;
+        default:
+          null;
+      }
+
+      // Replace placeholder text with actual data
+      newHtml = html.replace("%id%", obj.id);
+      newHtml = newHtml.replace("%description%", obj.description);
+      newHtml = newHtml.replace("%value%", obj.value);
+
+      // Insert HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
     // Return the DOMStrings object to make it publicly available through other controllers
     getDOMStrings: function() {
@@ -120,6 +146,7 @@ var controller = (function(budgetCtrl, uiCtrl) {
     // const { type, description, value } = input;
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // Add item to UI
+    uiCtrl.addListItem(newItem, input.type);
     // Calculate Budget
     // Display budget on UI
   };
